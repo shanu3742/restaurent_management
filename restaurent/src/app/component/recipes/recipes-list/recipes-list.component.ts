5d1bb4cb1641738ede41e3a9a638b424de503223
kumar shanu <kumar.shanu@stockedge.com>
2023-11-08 18:11:28 +0530

    recipes updated

--- restaurent/src/app/component/recipes/recipes-list/recipes-list.component.ts
+++ restaurent/src/app/component/recipes/recipes-list/recipes-list.component.ts
@@ -15,10 +15,16 @@ export class RecipesListComponent {
       image:'https://igav3-metcdn-com.global.ssl.fastly.net/content/uploads/sites/2/2022/12/05171743/MLA_French.png'
     },
     {
-      id: '01',
+      id: '02',
       name:'Idli' ,
       description: 'Idli is the  South Indian breakfast dish',
       image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnMdHtT7yykZlFgdzgHjfrJm0IwvjGaqK2Bg&usqp=CAU'
+    },
+    {
+      id: '02',
+      name:'Homemade Cannoli' ,
+      description: 'seriously strong coffee and a cannoli',
+      image:'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/4/24/2/ED0203_homemade-cannoli_s4x3.jpg.rend.hgtvcom.511.288.suffix/1433592011573.jpeg'
     }
   ];
   constructor(){}
