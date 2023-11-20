d0bf51c3bd8fb885875e2227482855d9957c67e0
kumar shanu <kumar.shanu@stockedge.com>
2023-11-20 18:01:55 +0530

    user updated based on routes

--- restaurent/src/app/component/demo-router/users/user/user.component.ts
+++ restaurent/src/app/component/demo-router/users/user/user.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy{
   user!: {id: number, name: string};
-
+  paramsSubscription!:Subscription
   constructor(private route:ActivatedRoute) { }
 
   ngOnInit() {
@@ -19,6 +20,17 @@ export class UserComponent implements OnInit {
     console.log(this.route.snapshot)
     this.user.id=this.route?.snapshot?.params['id']??'1';
     this.user.name=this.route?.snapshot?.params['name']??'agent';
+   this.paramsSubscription= this.route.params.subscribe((params:Params) => {
+      this.user.id = params['id'];
+      this.user.name= params['name']
+    })
+  }
+
+  ngOnDestroy(): void {
+    /**
+     * UNSUB to sub service
+     */
+    this.paramsSubscription.unsubscribe()
   }
 
 }
